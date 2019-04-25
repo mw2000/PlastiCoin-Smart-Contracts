804e1a5abbcc2291df13235e9391c3786ce63469
Mihir <mwadekar2000@gmail.com>
2019-04-25 15:22:53 -0700

    Reordered files and removed unnecessary folders

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -8,11 +8,4 @@ contract ERC20 {
     function transfer(address to, uint value) public returns (bool ok);
     function transferFrom(address from, address to, uint value) public returns (bool ok);
     function approve(address spender, uint value) public returns (bool ok);
-
-    //ERC 621 Functions
-    function increaseSupply(uint value, address to) public returns (bool);
-    function decreaseSupply(uint value, address from) public returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
 }
